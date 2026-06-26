e4ccca21150008d905e8f97258d57acf5b64448d
Hruthika Chavan <150798678+hruthikachavan@users.noreply.github.com>
2026-06-26 15:33:29 +0530

    Update server.js

--- backend/src/server.js
+++ backend/src/server.js
@@ -4,7 +4,16 @@ const app    = require('./app');
 const initDB = require('./config/initDB');
 
 const PORT = process.env.PORT || 5000;
+const { query } = require("./config/prisma");
 
+(async () => {
+  try {
+    const result = await query("SELECT NOW()");
+    console.log("Database connected:", result.rows[0]);
+  } catch (err) {
+    console.error("Database connection failed:", err);
+  }
+})();
 (async () => {
   await initDB();
   app.listen(PORT, () => {
